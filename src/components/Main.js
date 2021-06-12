0cf674263a9e3167fdfa9167c407429fd186f59e
thamo2020 <thamodya01@gmail.com>
2021-06-12 18:08:13 +0530

    finishing main grid with real data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,20 @@
+import { Divider, Typography } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
+import Markdown from 'markdown-to-jsx'
 import React from 'react'
+import { posts } from '../data/data'
 
-const Main = () => {
+const Main = ({title}) => {
     return (
-        <div>
-            Main 
-        </div>
+       <Grid item xs={12} md={8}>
+           <Typography variant="h6" gutterBottom>
+               {title}
+           </Typography>
+           <Divider />
+           {posts.map((post)=> (
+               <Markdown key={post.body}>{post.body}</Markdown>  //if you use real api ,then you can replace this from that real api
+           ))}
+       </Grid>
     )
 }
 
