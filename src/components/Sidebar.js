5cfdb44680395e3e0b1dbce58b4c96bd1e8175ed
thamo2020 <thamodya01@gmail.com>
2021-06-12 18:45:43 +0530

    finishing paper and archives part in the sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import { Link, Typography } from '@material-ui/core';
+import { Grid, Paper } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+import React from 'react';
 
-const Sidebar = () => {
+const useStyles = makeStyles((theme) => ({
+    sidebarAboutBox: {
+      padding: theme.spacing(2),
+    },
+    sidebarSection: {
+      marginTop: theme.spacing(3),
+    },
+  }));
+    
+
+export default function Sidebar  (props)  {
+    const classes = useStyles();
+    const {description,title,archives} = props;
+
     return (
-        <div>
-            sidebar
-        </div>
+        <Grid item xs={12} md={4}>
+            <Paper elevation={0} className={classes.sidebarAboutBox}>
+                <Typography variant="h6" gutterBottom >
+                    {title}
+                </Typography>
+                <Typography>{description}</Typography>
+            </Paper>
+
+            <Typography variant="h6" gutterBottom className={classes.sidebarSection}>
+                Archives
+            </Typography>
+            {archives.map((archive) => (
+                <Link
+                display="block"
+                variant="body1"
+                href={archive.url}
+                key={archive.title}
+                >
+                {archive.title}
+                </Link>
+      ))}
+        </Grid>
     )
 }
 
-export default Sidebar
+
